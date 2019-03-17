88ad50ced1bffe774e539ad504e01c517b5cd40c
tweeki99 <danil.shipilov.99@gmail.com>
2019-03-17 18:48:15 +0600

    hw15

---
+++ hw15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type consoleType = typeof(Console);
+            //MethodInfo[] consoleMethods = consoleType.GetMethods();             //1 задание
+
+            //foreach (MemberInfo memberInfo in consoleMethods)
+            //{
+            //        Console.WriteLine(memberInfo);
+            //}
+            
+
+
+            Book book = new Book { Name = "Тарас Бульба", Author = "Н.Гоголь" };
+
+            Type bookType = book.GetType();                   //2 задание
+
+            var bookProperties = bookType.GetRuntimeProperties();
+
+            foreach (var memberInfo in bookProperties)
+            {
+                Console.WriteLine(memberInfo.Name +": "+ memberInfo.GetValue(book));
+            }
+        }
+    }
+}
